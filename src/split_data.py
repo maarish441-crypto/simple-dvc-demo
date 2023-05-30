288577ed5e24ba342bb07cda8808c3efe560be2d
maarish441-crypto <maarish441@gmail.com>
2023-05-30 17:02:56 +0530

    tracker added

--- src/split_data.py
+++ src/split_data.py
@@ -12,7 +12,7 @@ def split_and_saved_data(config_path):
     split_ratio = config['split_data']['test_size']
     random_state = config['base']['random_state']
 
-    df = pd.read_csv(raw_data_path, sep = " , ")
+    df = pd.read_csv(raw_data_path, sep = ",")
     train , test= train_test_split(
         df,
         test_size = split_ratio,
@@ -28,4 +28,4 @@ if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config",default = "params.yaml")
     parsed_args = args.parse_args()
-    data = split_and_saved_data(config_path = parsed_args.config )
+    split_and_saved_data(config_path = parsed_args.config )
